55051b6c2ac9040cbd5fa79f7ac2261afa538c86
ChamodKalupahana <Chamodkalupahana@gmail.com>
2026-03-31 15:51:21 +0100

    fixed date

--- lighthouse-batch.js
+++ lighthouse-batch.js
@@ -53,7 +53,7 @@ function formatDateDDMMYYYY(date = new Date()) {
     const dd = String(date.getDate()).padStart(2, '0');
     const mm = String(date.getMonth() + 1).padStart(2, '0');
     const yyyy = date.getFullYear();
-    return `${dd}-${mm}-${yyyy}`;
+    return `${dd}/${mm}/${yyyy}`;
 }
 
 function resolveChromePath() {
